f2828e776af0667fc35fc99c0c625849d0d30b8c
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2025-06-09 21:24:08 +0200

    feat: improve performance of search url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [{
+      source: '/search',
+      destination: '/search/index.html'
+    }]
+  }
 };
 
 export default nextConfig;
